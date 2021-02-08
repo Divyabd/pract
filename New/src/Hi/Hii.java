7b2dd165d4b99a173f7fa01db32a7c1ea0df347e
Divyabd <divyagowda8393@gmail.com>
2021-02-08 09:48:37 +0530

    cc

--- New/src/Hi/Hii.java
+++ New/src/Hi/Hii.java
@@ -5,8 +5,8 @@ public class Hii {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is divya");
-		int a=10;
-		int b=9;
+		
+		System.out.println("This is divya");
 	}
 
 }
