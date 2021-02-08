7066ceb9c37713145425efed83de91ba318a424a
Divyabd <divyagowda8393@gmail.com>
2021-02-08 09:42:46 +0530

    1st commit

---
+++ New/src/Hi/Hii.java
@@ -0,0 +1,12 @@
+package Hi;
+
+public class Hii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is divya");
+		int a=10;
+		int b=9;
+	}
+
+}
